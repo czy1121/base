bd19764b5d47af84a30af20a8c19529844d8d9f7
ezy <249736246@qq.com>
2024-03-26 21:39:14 +0800

    #

--- base/src/main/java/me/reezy/cosmo/base/BaseActivity.kt
+++ base/src/main/java/me/reezy/cosmo/base/BaseActivity.kt
@@ -1,11 +1,17 @@
 package me.reezy.cosmo.base
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.ref.WeakReference
 
 abstract class BaseActivity(@LayoutRes contentLayoutId: Int) : AppCompatActivity(contentLayoutId), BaseView {
+
+    fun requireView(): View {
+        return findViewById<ViewGroup>(android.R.id.content).getChildAt(0)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
